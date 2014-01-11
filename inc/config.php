f2c5ff183ea6687cee6f000ccb7e38aa9071ed74
Jeremy Ward <jeremy@jmichaelward.com>
2014-01-11 13:05:25 -0600

    Adds ROOT_DIR constant and reconfigures BASE_URL constant to better accommodate for variable installation paths.

--- inc/config.php
+++ inc/config.php
@@ -1,5 +1,7 @@
 <?php
 require('session.php');
 
-define('BASE_URL','http://' . $_SERVER['SERVER_NAME'] . '/scorekeeper/');
+// Set your relative root directory for the project.
+define('ROOT_DIR','/web/scorekeeper/');
+define('BASE_URL','http://' . $_SERVER['SERVER_NAME'] . ROOT_DIR);
 
